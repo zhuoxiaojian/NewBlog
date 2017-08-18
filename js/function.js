ec3c72a688762de5c28f6ab9caf76eaf179481d1
zhuoxiaojian <1933860854@qq.com>
2017-08-18 17:16:23 +0800

    test

---
+++ js/function.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+	//ÏêÏ¸1
+	$('#id_more').mouseover(function(){
+		$('#id_more #ckxx_m1').animate({'top':'-34px'});
+		$('#id_more #ckxx_m2').animate({'top':'0px'});
+	});
+	$('#id_more').mouseleave(function(){
+		
+		$('#id_more #ckxx_m2').animate({'top':'-34px'});
+		$('#id_more #ckxx_m1').animate({'top':'0px'});
+	});
+
+});
+
+$(document).ready(function(){
+	//ÏêÏ¸2
+	$('#id_more2').mouseover(function(){
+		$('#id_more2 #ckxx_m21').animate({'top':'-34px'});
+		$('#id_more2 #ckxx_m22').animate({'top':'0px'});
+	});
+	$('#id_more2').mouseleave(function(){
+		
+		$('#id_more2 #ckxx_m22').animate({'top':'-34px'});
+		$('#id_more2 #ckxx_m21').animate({'top':'0px'});
+	});
+
+});
+
+$(document).ready(function(){
+	//ÏêÏ¸3
+	$('#id_more3').mouseover(function(){
+		$('#id_more3 #ckxx_m31').animate({'top':'-34px'});
+		$('#id_more3 #ckxx_m32').animate({'top':'0px'});
+	});
+	$('#id_more3').mouseleave(function(){
+		
+		$('#id_more3 #ckxx_m32').animate({'top':'-34px'});
+		$('#id_more3 #ckxx_m31').animate({'top':'0px'});
+	});
+
+});
+
